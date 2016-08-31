f869c05afcfefa1c4a2f0faf3f07019240e85468
Abhimanyu Dubey <abhimanyu1401@gmail.com>
2016-08-31 21:00:00 +0530

    moved source

--- code/vis_filter_bogus_images.py
+++ code/vis_filter_bogus_images.py
@@ -9,14 +9,14 @@ def rmse(predictions, targets):
 ref_image = np.asarray(caffe.io.load_image("/home/nikhil/data/placepulse2/images_original/TelAviv/32.131051_34.816062.JPG",color=True))
 image_pardir = "/home/nikhil/data/placepulse2/images_original/"
 csv_pardir = glob.glob("/home/dubeya/extrapolated_votes/safety/scores/*.csv")
-op_pardir = "/home/dubeya/extrapolated_votes/safety/scores_new/"
+op_pardir = "/home/dubeya/extrapolated_votes/safety/scores_new"
 
 if not os.path.exists(op_pardir):
     os.makedirs(op_pardir)
 
 for csvfile in csv_pardir:
     with open(csvfile,'r') as csv_src_file:
-        print "Reading csv",csvfile,"right now"
+        print "Reading csv",csvfile,"right now",os.path.basename(csv_src_file)
         op_file = os.path.join(op_pardir,os.path.basename(csv_src_file))
         with open(op_file,'w') as f:
             for line in csv_src_file:
