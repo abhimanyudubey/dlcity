bf3566dbbc28e1aa6f35fbc65be7b0a5c006a631
Abhimanyu Dubey <abhimanyu1401@gmail.com>
2017-03-09 16:35:01 -0500

    updates

--- src/rename_batch.py
+++ src/rename_batch.py
@@ -1,10 +1,10 @@
 import glob
 import os
 
-fs = glob.glob('/home/dubeya/dlcity/data/ref_images/*.JPG')
+fs = glob.glob('/home/dubeya/dlcity/data/images_ref/*.JPG')
 for file in fs:
     id = os.path.basename(file).split('_')[2]
-    pardir = '/home/dubeya/dlcity/data/ref_images/'
+    pardir = '/home/dubeya/dlcity/data/images_ref/'
     newname = pardir+id+'.JPG'
     cmd = 'mv %s %s' % (file, newname)
     os.system(cmd)
